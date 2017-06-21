9d46982a8e7bbd780d423f38b18dae65b7f999a5
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-06-21 15:58:19 +0300

    Commit

--- clients/Bootstrap/dist/assets/javascript/scripts.js
+++ clients/Bootstrap/dist/assets/javascript/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-	
+	//Mobile preview Iframe action
+	$('.btn-iframe-to-mobile-trigger').on('click', function(event) {
+		event.preventDefault();
+		$('.iframe-preview').addClass('iframe-preview--mobile');
+	});
+	$('.btn-iframe-to-desktop-trigger').on('click', function(event) {
+		event.preventDefault();
+		$('.iframe-preview').removeClass('iframe-preview--mobile');
+	});
+
 
 
 });
