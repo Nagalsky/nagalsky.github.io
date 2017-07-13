3bcd6a8e2fd22a31ee78b3bf5ca61b4632fd29bb
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-07-13 17:27:26 +0300

    Home page

--- clients/WhiteSmoke/dist/assets/javascript/scripts.js
+++ clients/WhiteSmoke/dist/assets/javascript/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
-	
+	//Right sidebar toggle action
+	$('.sidebar__right__toggle').on('click', function(event){
+		event.preventDefault();
+		$('.sidebar__right').toggleClass('sidebar__right--opened');
+	});
 
 
 });
