227a24387b059c773f2c489853e80725f4fd19ce
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-07-25 14:56:48 +0300

    Home page

--- clients/neomam-website/dist/assets/javascript/scripts.js
+++ clients/neomam-website/dist/assets/javascript/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-	
+	/* ======== SCROLL TO ELEMENT ======== */
+	$('[data-click=scroll-to-target]').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var target = $(this).attr('href');
+    var headerHeight = 55;
+    $('html, body').animate({
+        scrollTop: $(target).offset().top - headerHeight
+    }, 500);
+  });
 
 
 });
