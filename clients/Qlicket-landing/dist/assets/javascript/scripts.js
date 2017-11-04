ac53fa0b74dea5c238f07d90e569917615459737
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-11-04 14:21:00 +0200

    Fix

--- clients/Qlicket-landing/dist/assets/javascript/scripts.js
+++ clients/Qlicket-landing/dist/assets/javascript/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
 
-	
 
+	//Slick carousel initial
+	$('.carousel').slick({
+		arrows: true,
+		//autoplay: true,
+		autoplaySpeed: 3000,
+		draggable: false,
+		focusOnSelect: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		pauseOnDotsHover: false,
+		fade: true
+	});
+
+	$('.carousel-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.carousel',
+    dots: true,
+    centerMode: false,
+    focusOnSelect: true
+  });
 
 });
