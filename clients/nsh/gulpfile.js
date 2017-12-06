8e6c92333e7bddfc516d75e6e20217d533f22995
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-12-06 19:43:21 +0200

    fff

--- clients/nsh/gulpfile.js
+++ clients/nsh/gulpfile.js
@@ -85,7 +85,8 @@ gulp.task('javascript:vendors', task.javascript = function () {
     'node_modules/popper.js/dist/umd/popper.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/hiraku/src/hiraku.min.js',
-    'node_modules/slick-carousel/slick/slick.min.js'])
+    'node_modules/slick-carousel/slick/slick.min.js',
+    'node_modules/isotope-layout/dist/isotope.pkgd.min.js'])
     .pipe(concat('vendors.min.js'))
     .pipe(gulp.dest(path.build.javascript));
 });
--- clients/nsh/src/assets/javascript/scripts.js
+++ clients/nsh/src/assets/javascript/scripts.js
@@ -44,6 +44,69 @@ $(document).ready(function(){
 	    }
 	  ]
 	});
+	$('.modal-gallery').slick({
+		autoplay: false,
+		autoplaySpeed: 3000,
+		arrows: false,
+		dots: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		pauseOnDotsHover: false
+	});
+
+	$('.modal-gallery-thumbnails').slick({
+		autoplay: false,
+		autoplaySpeed: 3000,
+		arrows: false,
+		dots: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		pauseOnDotsHover: false,
+		slidesToShow: 5,
+  	slidesToScroll: 1,
+		focusOnSelect: true,
+		responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 4
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    }
+	  ]
+	});
+
+
+	//Isotope filtering initial
+	$('.filter-grid').isotope({
+    itemSelector: '.filter-item',
+    layoutMode: 'fitRows'
+  });
+	$('.filter-controls .btn').click(function () {
+    var selector = $(this).attr('data-filter');
+    $('.filter-grid').isotope({
+      filter: selector
+    });
+    return false;
+  });
+
+	//Bootstrap modal lightbox
+	$('.lightbox-toggle img').on('click', function () {
+    var image = $(this).attr('src');
+    $('#modal-lightbox').on('show.bs.modal', function () {
+      $(".modal__lightbox-img").attr("src", image);
+    });
+  });
+	$('.modal').on('shown.bs.modal', function (e) {
+		$('.modal-gallery, .modal-gallery-thumbnails').slick("setPosition", 0);
+  });
 
 
 });
