7fba644824abc16da74d965fba4cdccd070508c2
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-12-06 15:25:11 +0200

    WIll

---
+++ clients/nsh/src/assets/javascript/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	//Mobile menu action
+	$(".navbar-collapse").hiraku({
+    btn:".navbar-toggler",
+    direction:"right"
+	});
+
+	//Slick gallery initial
+	$('.gallery').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		arrows: false,
+		dots: true
+	});
+
+	$('.gallery-logos').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		arrows: false,
+		dots: false,
+		slidesToShow: 6,
+  	slidesToScroll: 6,
+		responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 4
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    }
+	  ]
+	});
+
+
+});
