7a8a423165d7b2d971337965d48a41202424fd83
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-12-19 22:11:34 +0200

    Commit

--- clients/morbly/src/assets/javascript/scripts.js
+++ clients/morbly/src/assets/javascript/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	//Header background color animation
+	//Header background color animation action
 	$(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
@@ -11,5 +11,14 @@ $(document).ready(function(){
     }
 	});
 
+	//Load more action
+	$("div.logos-item").slice(0, 1).show();
+	$(".btn-load-more").on('click', function (e) {
+		e.preventDefault();
+		$("div.logos-item:hidden").slice(0, 1).slideDown();
+		$('html,body').animate({
+				scrollTop: $(this).offset().top
+		}, 1200);
+	});
 
 });
