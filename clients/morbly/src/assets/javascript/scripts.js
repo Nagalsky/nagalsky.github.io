0b9a564110630c4a5d36578505edc5376df27a23
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2017-12-19 22:35:06 +0200

    Fix

--- clients/morbly/src/assets/javascript/scripts.js
+++ clients/morbly/src/assets/javascript/scripts.js
@@ -12,12 +12,15 @@ $(document).ready(function(){
 	});
 
 	//Load more action
-	$("div.logos-item").slice(0, 1).show();
+	$(".logos-item").slice(0, 1).show();
 	$(".btn-load-more").on('click', function (e) {
 		e.preventDefault();
-		$("div.logos-item:hidden").slice(0, 1).slideDown();
+		$(".logos-item:hidden").slice(0, 1).slideDown();
+		if ($('.logos-item:hidden').length === 0) {
+      $('.btn-load-more').replaceWith("<h3>No more logos</h3>");
+    }
 		$("html, body").animate({
-			scrollTop: $('.btn-load-more').offset().top
+			scrollTop: $('.btn-load-more').offset().top - 150
 		}, 1000);
 	});
 
