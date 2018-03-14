eec6d73f030dbfb2d20e8d2d13cb4133c529cc61
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-03-14 15:09:06 +0200

    ffff

--- clients/Ameritel/gulpfile.js
+++ clients/Ameritel/gulpfile.js
@@ -82,6 +82,7 @@ gulp.task('javascript:build', task.javascript = function () {
 gulp.task('javascript:vendors', task.javascript = function () {
   return gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/jquery.marquee/jquery.marquee.min.js',
     'node_modules/slick-carousel/slick/slick.min.js',
     'node_modules/popper.js/dist/umd/popper.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js'])
--- clients/Ameritel/src/assets/javascript/scripts.js
+++ clients/Ameritel/src/assets/javascript/scripts.js
@@ -18,4 +18,102 @@ $(document).ready(function(){
 	  ]
   });
 
+	$('.gallery-services').slick({
+	  slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		centerMode: true,
+  	centerPadding: '460px',
+	  responsive: [
+			{
+	      breakpoint: 1800,
+	      settings: {
+					centerPadding: '400px',
+	      }
+	    },
+			{
+	      breakpoint: 1600,
+	      settings: {
+					centerPadding: '360px',
+	      }
+	    },
+			{
+	      breakpoint: 1450,
+	      settings: {
+					centerPadding: '300px',
+	      }
+	    },
+			{
+	      breakpoint: 1300,
+	      settings: {
+					centerPadding: '260px',
+	      }
+	    },
+			{
+	      breakpoint: 1150,
+	      settings: {
+					centerPadding: '250px',
+	      }
+	    },
+			{
+	      breakpoint: 1050,
+	      settings: {
+					centerPadding: '220px',
+	      }
+	    },
+			{
+	      breakpoint: 960,
+	      settings: {
+					centerPadding: '200px',
+	      }
+	    },
+			{
+	      breakpoint: 860,
+	      settings: {
+					centerPadding: '180px',
+	      }
+	    },
+			{
+	      breakpoint: 790,
+	      settings: {
+					centerPadding: '120px',
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+			  	centerPadding: '0',
+	      }
+	    }
+	  ]
+  });
+
+	//Marquee initial
+	$('.marquee__line').marquee({
+		duration: 15000,
+    delayBeforeStart: 0,
+    direction: 'left'
+	});
+
+	if ($(window).width() < 767) {
+ 	 $('.marquee__line').marquee({
+ 		 duration: 10000,
+ 		 delayBeforeStart: 0,
+ 		 direction: 'left'
+ 	 });
+  };
+
+});
+
+$(window).resize(function() {
+ if ($(window).width() < 767) {
+	 $('.marquee__line').marquee({
+		 duration: 10000,
+		 delayBeforeStart: 0,
+		 direction: 'left'
+	 });
+ };
 });
