24a0a2c84f37edb0ca46b39068850b47162f61a3
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-04-16 19:01:42 +0300

    Fixes

--- clients/tbc/dist/assets/javascript/scripts.js
+++ clients/tbc/dist/assets/javascript/scripts.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$(".fieldset-hidden-toggle").change(function(){$(".fieldset-hidden").slideToggle(300)})});
--- clients/tbc/src/assets/javascript/scripts.js
+++ clients/tbc/src/assets/javascript/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
-	
+	/* ======== Show/hide hidden fields ======== */
+  $('.fieldset-hidden-toggle').change(function(){
+    $('.fieldset-hidden').slideToggle(300);
+  });
 
 });
