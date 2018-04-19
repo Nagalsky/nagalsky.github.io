e3fcbe8b05a42140eabf2ef15acb6dc0bb631440
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-04-19 16:43:16 +0300

    Fix navbar

--- clients/Eterna/dist/assets/javascript/scripts.js
+++ clients/Eterna/dist/assets/javascript/scripts.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".navbar-toggler").on("click",function(){$(".navbar").toggleClass("navbar--menu-opened")})});
+$(document).ready(function(){});
--- clients/Eterna/src/assets/javascript/scripts.js
+++ clients/Eterna/src/assets/javascript/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
 	//Add/remove background color for mobile Navbar
-	$('.navbar-toggler').on('click', function(){
+	/*$('.navbar-toggler').on('click', function(){
 		$('.navbar').toggleClass('navbar--menu-opened');
-	});
+	});*/
 
 });
