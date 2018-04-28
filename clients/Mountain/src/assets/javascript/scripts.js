58eb00d28b7cbfabda9166d80f6d47ccedb164b9
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-04-28 08:02:38 +0300

    Fixes

--- clients/Mountain/src/assets/javascript/scripts.js
+++ clients/Mountain/src/assets/javascript/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	/* ======== Navbar scroll animation ======== */
 	$(window).scroll(function() {
 	  var scroll = $(window).scrollTop();
 	  if (scroll >= 10) {
@@ -8,5 +9,22 @@ $(document).ready(function(){
 	      $(".navbar").removeClass("navbar--animated");
 	  }
 	});
+
+	/* ======== Scroll to section ======== */
+	$('[data-click=scroll-to-target]').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var target = $(this).attr('href');
+		if ($(window).width() < 991) {
+		  var navbarHeight = 79;
+		}
+		else {
+		  var navbarHeight = 97;
+		}
+    $('html, body').animate({
+        scrollTop: $(target).offset().top - navbarHeight
+    }, 500);
+		$('.navbar-collapse').removeClass('show');
+  });
 
 });
