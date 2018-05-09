c9f7cc09a7e2d3bfec5268d21f8308ead0e63060
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-05-09 11:46:11 +0300

    Headers

--- clients/Crypto/src/assets/javascript/scripts.js
+++ clients/Crypto/src/assets/javascript/scripts.js
@@ -1,8 +1,40 @@
 $(document).ready(function(){
 
-		$('.btn').on('click', function(e) {
+		//Switch dark/light themes
+		$('.btn-switch-themes-toggle').on('click', function(e) {
 			e.preventDefault();
 			$('html').toggleClass("theme-dark theme-light");
+			e.stopPropagation();
 		});
+
+		//Toggle expanded mobile search form
+		$('.btn-expanded-form-toggle').on('click', function(e) {
+			e.preventDefault();
+			$('.header__logo').toggleClass("header__logo--hidden");
+			$('.header__expanded-form-holder').toggleClass("header__expanded-form-holder--active");
+			$('.header__expanded-form').toggleClass("header__expanded-form--active");
+			e.stopPropagation();
+		});
+
+		//Close expanded mobile search form by click outside
+	  $(document).on("click touchstart", function(event) {
+	    if( $(event.target).closest('.header__expanded-form--active').length == 0 ) {
+	      $(".header__expanded-form--active").removeClass("header__expanded-form--active");
+				$('.header__logo').removeClass("header__logo--hidden");
+				$('.header__expanded-form-holder').removeClass("header__expanded-form-holder--active");
+				//$('.header__expanded-form .form-control').val('');
+	    }
+	  });
+
+		//Close expanded mobile search form by click on button
+		$('.btn-cancel').on('click', function(e) {
+			e.preventDefault();
+			$(".header__expanded-form--active").removeClass("header__expanded-form--active");
+			$('.header__logo').removeClass("header__logo--hidden");
+			$('.header__expanded-form-holder').removeClass("header__expanded-form-holder--active");
+			$('.header__expanded-form .form-control').val('');
+		});
+
+
 
 });
