7a8c67b79bb20fe179259510af244f68a7efebaf
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2018-11-01 18:18:42 +0200

    fff

--- clients/coopermoon/src/assets/javascript/scripts.js
+++ clients/coopermoon/src/assets/javascript/scripts.js
@@ -7,15 +7,18 @@ $(document).ready(function(){
 	  });
 
 		//Slick gallery initial
-		$('.gallery').slick();
+		$('.gallery').slick({
+			pauseOnFocus: false,
+			pauseOnHover: false
+		});
 
 		$(window).on('load resize', function () {
      changepos();
     });
 
     function changepos() {
-      var toppos = ($('.gallery-features .slick-active').find(".features__img-holder").height()/2);
-      $('.gallery-features .slick-arrow').css('top',toppos+'px');
+      var toppos = ($('.gallery-features .slick-active, .gallery-leadership .slick-active').find(".features__img-holder").height()/2);
+      $('.gallery-features .slick-arrow, .gallery-leadership .slick-arrow').css('top',toppos+'px');
     }
 
 		$('.gallery-features').slick({
@@ -24,6 +27,8 @@ $(document).ready(function(){
 			arrows: true,
 			slidesToShow: 4,
   		slidesToScroll: 1,
+			pauseOnFocus: false,
+			pauseOnHover: false,
 			responsive: [
 				{
 		      breakpoint: 991,
@@ -49,20 +54,39 @@ $(document).ready(function(){
 		$('.benefits').slick({
 			autoplay: true,
 			autoplaySpeed: 3000,
-			arrows: false,
-			slidesToShow: 3,
-  		slidesToScroll: 1,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			mobileFirst: true,
 			responsive: [
 		    {
 		      breakpoint: 767,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1,
-		        arrows: true
-		      }
+					settings: 'unslick'
 		    }
 		  ]
 		});
+
+		$('.gallery-leadership').slick({
+			autoplay: true,
+			autoplaySpeed: 3000,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			mobileFirst: true,
+			responsive: [
+		    {
+		      breakpoint: 767,
+					settings: 'unslick'
+		    }
+		  ]
+		}).on('afterChange',function(event){
+      var toppos = ($('.gallery-leadership .slick-active').find(".features__img-holder").height()/2);
+      $('.gallery-leadership .slick-arrow').css('top',toppos+'px');
+    }).trigger('afterChange');
 
 
 
