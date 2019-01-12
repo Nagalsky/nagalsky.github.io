f14bf226d744955dee9d766db1fcdc93655160e5
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-01-12 12:18:37 +0200

    fff

--- clients/Africa/gulpfile.js
+++ clients/Africa/gulpfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var gulp                   = require('gulp'),
-    sass                   = require('gulp-sass'),
-    autoprefixer           = require('gulp-autoprefixer'),
-    cleanCSS               = require('gulp-clean-css'),
-    uglify                 = require('gulp-uglify'),
-    pump                   = require('pump'),
-    concat                 = require('gulp-concat'),
-    rigger                 = require('gulp-rigger'),
-    imagemin               = require('gulp-imagemin'),
-    imageminJpegRecompress = require('imagemin-jpeg-recompress'),
-    imageminSvgo           = require('gulp-imagemin').svgo,
-    imageminPngquant       = require('imagemin-pngquant'),
-    browserSync            = require('browser-sync').create(),
-    watch                  = require('gulp-watch'),
-    del                    = require('del');
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  uglify = require('gulp-uglify'),
+  pump = require('pump'),
+  concat = require('gulp-concat'),
+  rigger = require('gulp-rigger'),
+  imagemin = require('gulp-imagemin'),
+  imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+  imageminSvgo = require('gulp-imagemin').svgo,
+  imageminPngquant = require('imagemin-pngquant'),
+  browserSync = require('browser-sync').create(),
+  watch = require('gulp-watch'),
+  del = require('del');
 
 var task = {};
 
@@ -45,66 +45,75 @@ var path = {
 var randomName = Math.random().toString(16).substring(2);
 
 // HTML
-gulp.task('html:build', task.html = function () {
+gulp.task('html:build', task.html = function() {
   gulp.src(path.src.html)
-  .pipe(rigger())
-  .pipe(gulp.dest(path.build.html))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
+    .pipe(rigger())
+    .pipe(gulp.dest(path.build.html))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 //Stylesheets
-gulp.task('sass:build', function () {
+gulp.task('sass:build', function() {
   return gulp.src(path.src.stylesheets)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(cleanCSS({compatibility: 'ie8'}))
-  .pipe(gulp.dest(path.build.stylesheets))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest(path.build.stylesheets))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // JAVASCRIPT
-gulp.task('javascript:build', task.javascript = function () {
+gulp.task('javascript:build', task.javascript = function() {
   gulp.src(path.src.javascript)
-  .pipe(uglify())
-  .pipe(gulp.dest(path.build.javascript))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
+    .pipe(uglify())
+    .pipe(gulp.dest(path.build.javascript))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('javascript:vendors', task.javascript = function () {
+gulp.task('javascript:vendors', task.javascript = function() {
   return gulp.src([
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/umd/popper.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/slick-carousel/slick/slick.min.js',
+      'node_modules/popper.js/dist/umd/popper.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ])
     .pipe(concat('vendors.min.js'))
     .pipe(gulp.dest(path.build.javascript));
 });
 
 // FONTS
-gulp.task('fonts:build', task.fonts = function () {
+gulp.task('fonts:build', task.fonts = function() {
   gulp.src(path.src.fonts)
-  .pipe(gulp.dest(path.build.fonts))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
+    .pipe(gulp.dest(path.build.fonts))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 
 //Images
-gulp.task('img:build', task.img = function () {
+gulp.task('img:build', task.img = function() {
   gulp.src(path.src.img)
     .pipe(imagemin([
-      imageminJpegRecompress({quality: 'low'}),
+      imageminJpegRecompress({
+        quality: 'low'
+      }),
       imageminSvgo(),
-      imageminPngquant({nofs: true, speed: 1})
+      imageminPngquant({
+        nofs: true,
+        speed: 1
+      })
     ]))
     .pipe(gulp.dest(path.build.img))
     .pipe(browserSync.reload({
@@ -115,7 +124,7 @@ gulp.task('img:build', task.img = function () {
 // Server
 gulp.task('server:build', function() {
   browserSync.init({
-    port : 3200,
+    port: 3200,
     server: {
       baseDir: "dist",
       routes: {
@@ -143,20 +152,20 @@ gulp.task('build', [
   'fonts:build'
 ]);
 
-gulp.task('watch', function () {
-  watch([path.watch.stylesheets], function (event, cb) {
+gulp.task('watch', function() {
+  watch([path.watch.stylesheets], function(event, cb) {
     gulp.start('sass:build');
   });
-  watch([path.watch.html], function (event, cb) {
+  watch([path.watch.html], function(event, cb) {
     gulp.start('html:build');
   });
-  watch([path.watch.img], function (event, cb) {
+  watch([path.watch.img], function(event, cb) {
     gulp.start('img:build');
   });
-  watch([path.watch.javascript], function (event, cb) {
+  watch([path.watch.javascript], function(event, cb) {
     gulp.start('javascript:build');
   });
-  watch([path.watch.fonts], function (event, cb) {
+  watch([path.watch.fonts], function(event, cb) {
     gulp.start('fonts:build');
   });
 });
--- clients/Africa/src/assets/javascript/scripts.js
+++ clients/Africa/src/assets/javascript/scripts.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
     }
   });
 
+  //Hero gallery
+  $('.hero__gallery').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    appendDots: $(".hero__navigation"),
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false
+  });
+
+  $('.hero__gallery__pause').on('click', function() {
+    $('.hero__gallery').slick('slickPause');
+    $(this).addClass('d-none');
+    $('.hero__gallery__play').removeClass('d-none');
+  });
+
+  $('.hero__gallery__play').on('click', function() {
+    $('.hero__gallery').slick('slickPlay');
+    $(this).addClass('d-none');
+    $('.hero__gallery__pause').removeClass('d-none');
+  });
+
 });
