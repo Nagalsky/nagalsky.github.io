0405232aa3e24d3c59ca3100197d123a06e3c528
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-01-30 15:00:33 +0200

    start

---
+++ clients/react/src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+import Container from './Container'
+import { breakpoints } from '../lib/breakpoints'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <Container>
+          <h1
+            css={css`
+              ${breakpoints.xsDown} {
+                display: none;
+              }
+            `}
+          >
+            I'm a header
+          </h1>
+        </Container>
+      </header>
+    )
+  }
+}
+
+export default Header
