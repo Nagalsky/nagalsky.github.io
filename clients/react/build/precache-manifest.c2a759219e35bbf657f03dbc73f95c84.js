84746ff650a1426d17db602f26c5c69f1ff12f45
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-01-31 11:17:40 +0200

    fff

---
+++ clients/react/build/precache-manifest.c2a759219e35bbf657f03dbc73f95c84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/logo.4009eef3.svg"
+  },
+  {
+    "revision": "2f5711820d94c7976a06b1179ef52161",
+    "url": "/static/media/lato-regular-webfont.2f571182.woff"
+  },
+  {
+    "revision": "ddc1c1a9983235396bd50e2a0f16a389",
+    "url": "/static/media/lato-light-webfont.ddc1c1a9.woff"
+  },
+  {
+    "revision": "e2b04827f1c49ea35712ac55c8a053a1",
+    "url": "/static/media/lato-bold-webfont.e2b04827.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbf307e65259acaaa9f0",
+    "url": "/static/js/main.fbf307e6.chunk.js"
+  },
+  {
+    "revision": "1219b6ac5fec56e62d20",
+    "url": "/static/js/1.1219b6ac.chunk.js"
+  },
+  {
+    "revision": "fbf307e65259acaaa9f0",
+    "url": "/static/css/main.a729e93b.chunk.css"
+  },
+  {
+    "revision": "ded9222e8090ff63ad697fb6bffa6177",
+    "url": "/index.html"
+  }
+];
