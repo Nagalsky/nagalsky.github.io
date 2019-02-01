aeedb360c8f8780a0808bf355e8e1d167ad80f95
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-02-01 15:18:45 +0200

    js

--- clients/Cargodesk/src/assets/js/scripts.js
+++ clients/Cargodesk/src/assets/js/scripts.js
@@ -58,4 +58,60 @@ $(document).ready(function() {
       .next('.collapse-box__body')
       .slideToggle(300)
   })
+
+  //Scroll to element
+  $("a[data-scroll]").click(function(e) {
+    e.preventDefault();
+    //Set Offset Distance from top to account for fixed nav
+    var offset = 119;
+    $(window).resize(function() {
+      if ($(window).width() < 992) {
+        offset = 82;
+      }
+    });
+    if ($(window).width() < 992) {
+      offset = 82;
+    }
+    var target = "#" + $(this).data("scroll");
+    var $target = $(target);
+    //Animate the scroll to, include easing lib if you want more fancypants easings
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top - offset
+        },
+        800,
+        "swing"
+      );
+
+    $(".navbar-toggler").addClass("collapsed");
+    $(".navbar-collapse").removeClass("show");
+  });
+
+  //Cargo form action
+  $('#continue-details').on('click', function(event) {
+    event.preventDefault();
+    $(this).addClass('d-none')
+    $('#cargo-details .display-none').slideToggle(300);
+  })
+
+  $('#add-new-shipment').on('click', function(event) {
+    event.preventDefault();
+    $('.shipment-item:first').clone().removeClass('shipment-item-prev').appendTo('#shipments').prev().addClass('shipment-item-prev');
+  })
+
+  $(document).on('click', '.btn-icon--delete', function () {
+  	$(this).parents('.shipment-item').remove();
+  });
+
+  // $('.btn-icon--delete').on('click', function(event) {
+  //   event.preventDefault();
+  //   $(this).parents('.shipment-item').remove();
+  // })
+
+
+
+
+
 })
