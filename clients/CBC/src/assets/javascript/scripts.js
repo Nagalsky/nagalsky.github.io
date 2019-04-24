bd37e0e4350a89b0da73f71b8c715e1633073ee1
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-04-24 21:41:34 +0300

    fwfwfw

--- clients/CBC/src/assets/javascript/scripts.js
+++ clients/CBC/src/assets/javascript/scripts.js
@@ -1,14 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //Menu action
   var $menu = $('#menu'),
     $menuToggle = $('.header__menu-toggle'),
     $menuClose = $('.menu__close')
-  $($menuToggle).on('click', function(e) {
+  $($menuToggle).on('click', function (e) {
     e.preventDefault()
     $menu.toggleClass('active')
   })
-  $($menuClose).on('click touchstart', function(e) {
+  $($menuClose).on('click touchstart', function (e) {
     e.preventDefault()
     $menu.removeClass('active')
   })
+
+  //Initial event slick gallery
+  $('.events').slick({
+    autoplay: true,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 })
