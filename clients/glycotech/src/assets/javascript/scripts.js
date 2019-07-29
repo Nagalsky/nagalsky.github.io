8efa3f892495b1ac63e03120deb93b44292088ca
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-07-29 11:38:12 +0300

    wf2f

--- clients/glycotech/src/assets/javascript/scripts.js
+++ clients/glycotech/src/assets/javascript/scripts.js
@@ -74,13 +74,18 @@ $(document).ready(function () {
 
 
   //Collapsed form action(such as checkout)
+  var headerHeight = 170;
+  if ($(window).width() <= 991) {
+    headerHeight = 20;
+  }
+
   $('.fieldset-header__next-btn').on('click', function (e) {
     e.preventDefault()
     $(this).parent().parent().removeClass('fieldset-is-opened fieldset-is-edit').addClass('fieldset-is-finished');
     $(this).parent().parent().next('.fieldset-is-closed').removeClass('fieldset-is-closed').addClass('fieldset-is-opened');
     $(this).parent().parent().next('.fieldset-is-finished').removeClass('fieldset-is-finished').addClass('fieldset-is-opened');
     $([document.documentElement, document.body]).animate({
-      scrollTop: $(".fieldset-is-opened").offset().top - 30
+      scrollTop: $(".fieldset-is-opened").offset().top - headerHeight
     }, 600);
   })
 
@@ -90,7 +95,7 @@ $(document).ready(function () {
     $(this).parent().parent().parent().find('.fieldset-is-edit').removeClass('fieldset-is-edit');
     $(this).parent().parent().addClass('fieldset-is-edit');
     $([document.documentElement, document.body]).animate({
-      scrollTop: $(".fieldset-is-edit").offset().top - 30
+      scrollTop: $(".fieldset-is-edit").offset().top - headerHeight
     }, 600);
   })
 
@@ -111,11 +116,6 @@ $(document).ready(function () {
 })
 
 
-// $(window).on('resize', function () {
-//   $('.carousel-nav').css({
-//     'height': ($(".carousel-main .flickity-viewport").height() + 'px')
-//   });
-// });
 
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
