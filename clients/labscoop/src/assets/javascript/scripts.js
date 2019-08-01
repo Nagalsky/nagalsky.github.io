7ee3375ee691a9e944ce7229ee32b7cf99633715
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-08-01 17:23:20 +0300

    dwdw

--- clients/labscoop/src/assets/javascript/scripts.js
+++ clients/labscoop/src/assets/javascript/scripts.js
@@ -4,15 +4,18 @@ $(document).ready(function () {
   var cartSlidePanelToggle = $('.slide-panel-toggle');
   var cartSlidePanelMask = $('.slide-panel-mask');
   var cartSlidePanelParent = $('body');
+  var cartSlidePanelClose = $('.side-menu-close');
   $(cartSlidePanelToggle).on('click', function () {
     cartSlidePanelHolder.toggleClass('is-opened');
+    cartSlidePanelClose.toggleClass('is-active');
     cartSlidePanelMask.toggleClass('is-opened');
     cartSlidePanelParent.toggleClass('modal-open');
   });
-  $('.side-menu-close').on('click touchstart', function () {
+  $(cartSlidePanelClose).on('click touchstart', function () {
     cartSlidePanelHolder.removeClass('is-opened');
     cartSlidePanelMask.removeClass('is-opened');
     cartSlidePanelParent.removeClass('modal-open');
+    cartSlidePanelClose.removeClass('is-active');
   });
 
   $(document).on("click touchstart", function (e) {
@@ -23,8 +26,25 @@ $(document).ready(function () {
       cartSlidePanelHolder.removeClass('is-opened');
       cartSlidePanelMask.removeClass('is-opened');
       cartSlidePanelParent.removeClass('modal-open');
+      cartSlidePanelClose.removeClass('is-active');
     }
   });
 
+  //Header menu action
+  var headerMenu = $('.header-menu');
+  var headerMenuToggle = $('.header-menu__toggle');
+  var headerMenuClose = $('.header-menu__close');
+  var headerMenuParent = $('body');
+  headerMenuToggle.on('click', function () {
+    headerMenu.toggleClass('is-opened');
+    headerMenuParent.toggleClass('overflow-hidden');
+  })
+  headerMenuClose.on('click', function () {
+    headerMenu.removeClass('is-opened');
+    headerMenuParent.removeClass('overflow-hidden');
+  })
+
+  //Custom scrollbar initial
+  $('.header-menu').mCustomScrollbar();
 
 })
