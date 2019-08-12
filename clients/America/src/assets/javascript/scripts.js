a6acf1d279f17aa8f91eac18700ddec53fad9c90
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-08-12 21:14:07 +0300

    cwcwcw

--- clients/America/src/assets/javascript/scripts.js
+++ clients/America/src/assets/javascript/scripts.js
@@ -2,10 +2,18 @@ $(document).ready(function () {
   //Mobile menu action
   var $menu = $('.header__right')
   var $menuToggle = $('.header__menu-toggle')
+  var $menuClose = $('.header__menu-close')
   var $menuMask = $('.header__mask')
-  $menuToggle.on('click', function () {
+  $menuToggle.on('click', function (e) {
+    e.preventDefault()
     $menu.toggleClass('is-menu-opened')
     $menuMask.toggleClass('is-active')
+  })
+
+  $menuClose.on('click', function (e) {
+    e.preventDefault()
+    $menu.removeClass('is-menu-opened')
+    $menuMask.removeClass('is-active')
   })
 
   $(document).on('click touchstart', function (e) {
