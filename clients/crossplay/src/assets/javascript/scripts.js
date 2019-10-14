10d14ac9a1990453bb28cecaaf2a3f21515ea626
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-10-14 15:50:06 +0300

    cwcwc

--- clients/crossplay/src/assets/javascript/scripts.js
+++ clients/crossplay/src/assets/javascript/scripts.js
@@ -33,4 +33,47 @@ $(document).ready(function () {
     }
   })
 
+  //Click event to scroll to top
+  $('.footer__logo').click(function (e) {
+    e.preventDefault()
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+  //Initial range slider
+  $(".range-slider").ionRangeSlider({
+    postfix: 'K',
+    hide_min_max: true,
+    from_min: '40',
+  });
+
+  //Scroll to section action
+  $("a[data-scroll]").click(function (e) {
+    e.preventDefault();
+    //Set Offset Distance from top to account for fixed nav
+    var offset = 85;
+    $(window).resize(function () {
+      if ($(window).width() < 992) {
+        offset = 71;
+      }
+    });
+    if ($(window).width() < 992) {
+      offset = 71;
+    }
+    var target = "#" + $(this).data("scroll");
+    var $target = $(target);
+    //Animate the scroll to, include easing lib if you want more fancypants easings
+    $("html, body")
+      .stop()
+      .animate({
+          scrollTop: $target.offset().top - offset
+        },
+        800,
+        "swing"
+      );
+  });
+
+
 })
