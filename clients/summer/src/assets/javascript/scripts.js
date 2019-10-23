365d1e9cd7c4d100b58d0f20a8bfff1c0beb6252
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-10-23 11:41:07 +0300

    Fixes, new pages.

--- clients/summer/src/assets/javascript/scripts.js
+++ clients/summer/src/assets/javascript/scripts.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
 
   //Menu action
-  $menu = $('.menu');
-  $body = $('body');
-  $menuToggle = $('.menu__toggle');
-  $menuClose = $('.menu__close');
+  (function () {
+    $menu = $('.menu');
+    $body = $('body');
+    $menuToggle = $('.menu__toggle');
+    $menuClose = $('.menu__close');
 
-  $menuToggle.on('click', function (e) {
-    e.preventDefault()
-    $menu.toggleClass('is-opened')
-    $body.toggleClass('overflow-hidden')
-  })
-  $menuClose.on('click', function (e) {
-    e.preventDefault()
-    $menu.removeClass('is-opened')
-    $body.toggleClass('overflow-hidden')
-  })
+    $menuToggle.on('click', function (e) {
+      e.preventDefault()
+      $menu.toggleClass('is-opened')
+      $body.toggleClass('overflow-hidden')
+    })
+    $menuClose.on('click', function (e) {
+      e.preventDefault()
+      $menu.removeClass('is-opened')
+      $body.toggleClass('overflow-hidden')
+    })
+  }());
 
-  //Stop youtube video once modal is active
-  $('.modal').on('hidden.bs.modal', function () {
-    $(".modal iframe").attr("src", $(".modal iframe").attr("src"));
-  });
+
+  //Stop/Play youtube video once modal is active
+  $(function () {
+    var $modalVideo = $("#modal-video");
+    var $modalVideoUrl = $modalVideo.attr('src');
+
+    /* Remove iframe src attribute on page load to
+    prevent autoplay in background */
+    $modalVideo.attr('src', '');
+
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed */
+    $(".modal").on('shown.bs.modal', function () {
+      $modalVideo.attr('src', $modalVideoUrl);
+    });
+
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $(".modal").on('hide.bs.modal', function () {
+      $modalVideo.attr('src', '');
+    });
+  }());
+
 
 
   $('.wrapper').css({
