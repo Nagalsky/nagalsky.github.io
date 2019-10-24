0c6c3588e0c128183a0c3a201e872e7d2660c5e5
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-10-24 10:19:35 +0300

    fixes and new features

--- clients/summer/src/assets/javascript/scripts.js
+++ clients/summer/src/assets/javascript/scripts.js
@@ -20,27 +20,12 @@ $(document).ready(function () {
   }());
 
 
-  //Stop/Play youtube video once modal is active
-  $(function () {
-    var $modalVideo = $("#modal-video");
-    var $modalVideoUrl = $modalVideo.attr('src');
+  //Stop play youtube video once modal is closed
+  $('.modal').on('hide.bs.modal', function () {
+    var memory = $(this).html();
+    $(this).html(memory);
+  })
 
-    /* Remove iframe src attribute on page load to
-    prevent autoplay in background */
-    $modalVideo.attr('src', '');
-
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed */
-    $(".modal").on('shown.bs.modal', function () {
-      $modalVideo.attr('src', $modalVideoUrl);
-    });
-
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $(".modal").on('hide.bs.modal', function () {
-      $modalVideo.attr('src', '');
-    });
-  }());
 
 
 
