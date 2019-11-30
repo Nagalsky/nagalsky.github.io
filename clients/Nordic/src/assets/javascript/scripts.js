e3af8be674c9be599cab6efc1d502dcb6e3a47ad
Nikita Nagalsky <nikita@ahub.com>
2019-11-30 10:55:36 +0200

    initial commit

---
+++ clients/Nordic/src/assets/javascript/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  //Initial slick slider
+  $('.hero-gallery').slick({
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+})
