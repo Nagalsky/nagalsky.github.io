f7e04eaba5c3db4136cf14b9dd9e9d9a472076b2
Nikita Nagalsky <nikita@ahub.com>
2020-06-25 13:00:36 +0300

    new project

---
+++ clients/qarqish/assets/src/js/main.js
@@ -0,0 +1,25 @@
+// Import jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Import Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Import Bootstrap 4 js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/toast.js
+
+// Import Sticky top jQuery plugin
+//= ../../../node_modules/sticky-kit/dist/sticky-kit.min.js
+
+// Import custom js-files
+//= script.js
---
+++ clients/qarqish/assets/src/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  //Initial sticky header
+  $(".header").stick_in_parent();
+});
