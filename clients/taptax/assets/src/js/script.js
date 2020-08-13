c98a6454ab5318cc6a6afef2ac2ab0f5f3d478c1
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-08-13 14:28:27 +0300

    gefefe

---
+++ clients/taptax/assets/src/js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  // init: false,
+  navigation: {
+    nextEl: ".gallery__button-next",
+    prevEl: ".gallery__button-prev"
+  },
+  loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  breakpoints: {
+    540: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    }
+  }
+});
