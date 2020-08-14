74f90c795b92df867255ffd7c7df7571959a6aeb
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-08-14 11:02:06 +0300

    vwveve

---
+++ clients/taptax-blog/assets/src/js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  //Scroll to element
+  $("[data-scroll]").click(function(e) {
+    e.preventDefault();
+    var offset = 181;
+    $(window).resize(function() {
+      if ($(window).width() < 992) {
+        offset = 64;
+      }
+    });
+    if ($(window).width() < 992) {
+      offset = 64;
+    }
+    var aid = $(this).attr("href");
+    $("html,body").animate({ scrollTop: $(aid).offset().top - offset }, "slow");
+  });
+
+  //Close mobile menu once you click on the link
+  $(".navbar .nav-link").on("click", function() {
+    $(".navbar-collapse").removeClass("show");
+  });
+
+  $(".modal").on("shown.bs.modal", function(e) {
+    swiper2.update();
+  });
+});
+
+//Swiper gallery
+var swiper = new Swiper(".gallery", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".gallery__button-blog-next",
+    prevEl: ".gallery__button-blog-prev"
+  },
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 35
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 70
+    }
+  }
+});
+
+//Swiper modal blog gallery
+var swiper2 = new Swiper(".modal-gallery", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".modal-blog-gallery-btn-next",
+    prevEl: ".modal-blog-gallery-btn-prev"
+  },
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
+});
