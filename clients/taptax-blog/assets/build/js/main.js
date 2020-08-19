9d533dbac133ceb6d2fc66308a9a31d1e96aca5d
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-08-19 13:03:16 +0300

    vevevev

--- clients/taptax-blog/assets/build/js/main.js
+++ clients/taptax-blog/assets/build/js/main.js
@@ -18536,9 +18536,10 @@ var swiper2 = new Swiper(".modal-gallery", {
 //Swiper modal blog gallery
 var swiper3 = new Swiper(".phone-gallery", {
   slidesPerView: 1,
-  loop: true
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false
-  // }
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  }
 });
