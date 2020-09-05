58dc215687cae407590488a4f33b7f11b80280f3
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-09-05 09:43:05 +0300

    new page

--- clients/core/assets/build/js/main.js
+++ clients/core/assets/build/js/main.js
@@ -18461,3 +18461,18 @@ var swiper = new Swiper(".gallery", {
     disableOnInteraction: false
   }
 });
+
+//Swiper hero gallery
+var swiper = new Swiper(".hero-gallery", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".hero-gallery__button-next",
+    prevEl: ".hero-gallery__button-prev"
+  },
+  autoHeight: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
+});
