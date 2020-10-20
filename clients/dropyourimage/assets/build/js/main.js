e1c1b353ce2264c76b9f592e4b85e707287780aa
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-10-20 17:45:42 +0300

    new page

--- clients/dropyourimage/assets/build/js/main.js
+++ clients/dropyourimage/assets/build/js/main.js
@@ -18489,74 +18489,6 @@ $(document).ready(function() {
     $(".navigation").removeClass("navigation--is-opened");
   });
 
-  //Bar chart initial
-  var _barChart = function() {
-    var options = {
-      series: [
-        {
-          name: "Revenue",
-          data: [76, 85, 101, 98, 87]
-        }
-      ],
-      chart: {
-        type: "bar",
-        height: 229,
-        toolbar: {
-          show: false
-        }
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: "40%"
-        }
-      },
-      title: {
-        text: "Aantal afbeeldingen per maand",
-        align: "left",
-        margin: 10,
-        offsetX: 0,
-        offsetY: 0,
-        floating: false,
-        style: {
-          fontSize: "16px",
-          fontWeight: "normal",
-          fontFamily: "OpenSans-Regular",
-          color: "#474747"
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        show: true,
-        width: 2
-      },
-      colors: ["#152339"],
-      xaxis: {
-        categories: ["Aug.", "Sep.", "Okt.", "Nov.", "Dec."],
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        }
-      },
-      fill: {
-        opacity: 1
-      },
-      grid: {
-        show: true,
-        borderColor: "#707070"
-      }
-    };
-
-    var chart = new ApexCharts(document.querySelector("#chart-bar"), options);
-    chart.render();
-  };
-
-  _barChart();
-
   //Initial Bootstrap popover
   $(function() {
     $('[data-toggle="popover"]').popover();
@@ -18565,5 +18497,10 @@ $(document).ready(function() {
   //Initial Bootstrap select
   $(function() {
     $(".selectpicker").selectpicker();
+  });
+
+  //Initial Bootstrap tooltip
+  $(function() {
+    $('[data-toggle="tooltip"]').tooltip();
   });
 });
