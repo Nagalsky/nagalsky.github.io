d573a1493a841bc4e2f5f061c67a790747cd79ff
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-10-21 13:16:39 +0300

    new page

--- clients/dropyourimage/assets/src/js/script.js
+++ clients/dropyourimage/assets/src/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   //Mobile menu action
-  $(".navigation__toggle").on("click", function(e) {
-    e.preventDefault();
-    $(".navigation").toggleClass("navigation--is-opened");
-    $(".navigation__overlay").toggleClass("navigation__overlay--is-opened");
-  });
+  $(function() {
+    $(".navigation__toggle").on("click", function(e) {
+      e.preventDefault();
+      $(".navigation").toggleClass("navigation--is-opened");
+      $(".navigation__overlay").toggleClass("navigation__overlay--is-opened");
+    });
 
-  $(".navigation__overlay").on("click", function(e) {
-    e.preventDefault();
-    $(this).toggleClass("navigation__overlay--is-opened");
-    $(".navigation").removeClass("navigation--is-opened");
+    $(".navigation__overlay").on("click", function(e) {
+      e.preventDefault();
+      $(this).toggleClass("navigation__overlay--is-opened");
+      $(".navigation").removeClass("navigation--is-opened");
+    });
   });
 
   //Initial Bootstrap popover
@@ -31,5 +33,20 @@ $(document).ready(function() {
   $(function() {
     var divHeight = $("[data-get-heading-height]").height();
     $("[data-set-product-height]").css({ maxHeight: "calc(100vh - 260px - " + divHeight + "px)" });
+  });
+
+  //Box collapse open/close state
+  $(function() {
+    $(".box-collapse__toggle").on("click", function(e) {
+      e.preventDefault();
+      $(this)
+        .parent()
+        .toggleClass("is-opened");
+    });
+  });
+
+  $(function() {
+    var divHeight = $("[data-get-heading-height]").height();
+    $("[data-set-box-collapse-height]").css({ maxHeight: "calc(100vh - 188px - " + divHeight + "px)" });
   });
 });
