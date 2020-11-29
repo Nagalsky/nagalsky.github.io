5aef0a44e5e6a13ca53be1a98bdc10f49f350c26
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-11-29 16:57:56 +0200

    new page

--- clients/Fan/assets/src/js/main.js
+++ clients/Fan/assets/src/js/main.js
@@ -7,6 +7,9 @@
 // Import Swiper
 //= ../../../node_modules/swiper/swiper-bundle.min.js
 
+// Import Rangeslider
+//= ../../../node_modules/rangeslider.js/dist/rangeslider.min.js
+
 // Import Bootstrap 4 js
 //= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/alert.js
