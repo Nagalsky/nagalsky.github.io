bc001b88050491ada78ea489e2515365164b770a
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2020-12-08 12:31:44 +0200

    added modal

--- clients/genie/assets/src/js/script.js
+++ clients/genie/assets/src/js/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
     //Initial Bootstrap select
     $('.selectpicker').selectpicker();
+
+    //Select all checkboxes in the table
+    $(function(){
+        //column checkbox select all or cancel
+        $("input.select-all").click(function () {
+            var checked = this.checked;
+            $("input.select-item").each(function (index,item) {
+                item.checked = checked;
+            });
+        });
+
+        //check selected items
+        $("input.select-item").click(function () {
+            var checked = this.checked;
+            console.log(checked);
+            checkSelected();
+        });
+
+        //check is all selected
+        function checkSelected() {
+            var all = $("input.select-all")[0];
+            var total = $("input.select-item").length;
+            var len = $("input.select-item:checked:checked").length;
+            all.checked = len===total;
+        }
+    });
 })
