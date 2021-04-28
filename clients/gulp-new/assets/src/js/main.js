e6e9de584dbbdd960e957eb9d81a860cd89100ca
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-04-28 11:03:10 +0300

    added new gulp

---
+++ clients/gulp-new/assets/src/js/main.js
@@ -0,0 +1,11 @@
+// Import Popper
+//= ../../../node_modules/@popperjs/core/dist/umd/popper.min.js
+
+// Import Bootstrap 5
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+// Import Swiper js
+//= ../../../node_modules/swiper/swiper-bundle.min.js
+
+// Import custom js-files
+//= gallery.js
