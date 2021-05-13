dd9d8bc74ee4e8120841c4479481a92898359627
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-05-13 18:11:54 +0300

    added params to lazyload

--- clients/discount/assets/build/js/main.js
+++ clients/discount/assets/build/js/main.js
@@ -178,6 +178,9 @@ $(document).ready(function () {
 $(function () {
   $(".lazy-img").Lazy({
     effect: "fadeIn",
+    effectTime: 300,
+    threshold: 0,
+    placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
   });
 });
 //# sourceMappingURL=main.js.map
--- clients/discount/assets/src/js/components/lazy.js
+++ clients/discount/assets/src/js/components/lazy.js
@@ -1,5 +1,8 @@
 $(function () {
   $(".lazy-img").Lazy({
     effect: "fadeIn",
+    effectTime: 300,
+    threshold: 0,
+    placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
   });
 });
