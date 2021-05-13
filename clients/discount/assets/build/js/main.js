c883df35a7fa18f8115183bbf73f8b3fea317a79
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-05-13 18:13:45 +0300

    daily updates

--- clients/discount/assets/build/js/main.js
+++ clients/discount/assets/build/js/main.js
@@ -180,7 +180,6 @@ $(function () {
     effect: "fadeIn",
     effectTime: 300,
     threshold: 0,
-    placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
   });
 });
 //# sourceMappingURL=main.js.map
--- clients/discount/assets/src/js/components/lazy.js
+++ clients/discount/assets/src/js/components/lazy.js
@@ -3,6 +3,5 @@ $(function () {
     effect: "fadeIn",
     effectTime: 300,
     threshold: 0,
-    placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
   });
 });
