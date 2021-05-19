cb3064ed94c7c5e5904f61f0f88a941a39349f8f
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-05-19 21:36:46 +0300

    tailwind

---
+++ clients/tailwindcss-boilerplate-main/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,css}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "540px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1199px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
