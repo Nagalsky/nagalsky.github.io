37f66dd3348210193caadc0dafd812f1e2cdd3d1
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-05-25 09:54:29 +0300

    added new page

--- clients/discount/assets/build/js/main.js
+++ clients/discount/assets/build/js/main.js
@@ -283,28 +283,50 @@ $(document).ready(function () {
   })();
 });
 (function () {
-  const swiper = new Swiper(".related-gallery", {
-    loop: true,
-    navigation: {
-      nextEl: ".related-gallery-button--next",
-      prevEl: ".related-gallery-button--prev",
-    },
-    slidesPerView: 2,
-    spaceBetween: 10,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+  // const swiper = new Swiper(".related-gallery", {
+  //   loop: true,
+  //   navigation: {
+  //     nextEl: ".related-gallery-button--next",
+  //     prevEl: ".related-gallery-button--prev",
+  //   },
+  //   slidesPerView: 2,
+  //   spaceBetween: 10,
+  //   breakpoints: {
+  //     640: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 15,
+  //     },
+  //     768: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 15,
+  //     },
+  //     992: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 15,
+  //     },
+  //   },
+  // });
+
+  $(".related-gallery").slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
       },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 20,
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
       },
-      992: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-    },
+    ],
   });
 })();
 (function () {
