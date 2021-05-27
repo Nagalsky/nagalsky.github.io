7f6ec0c174824cb681fa94efd1912e5f8ed1ace0
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-05-27 14:37:18 +0300

    one more thing

--- clients/discount/assets/src/js/components/share-menu.js
+++ clients/discount/assets/src/js/components/share-menu.js
@@ -2,31 +2,12 @@ $(document).ready(function () {
   (function () {
     const $menu = $(".share-product-menu");
     const $menuToggle = $(".share-product-btn-open");
-    const shareButton = document.querySelector(".share-product-btn-open");
+    // const shareButton = document.querySelector(".share-product-btn-open");
     const $menuClose = $(".share-product-btn-close");
     const $menuMask = $(".share-product-mask");
     const $body = $("body");
 
-    shareButton.addEventListener("click", (event) => {
-      if (navigator.share) {
-        navigator
-          .share({
-            title: "WebShare API Demo",
-            url: "https://codepen.io/ayoisaiah/pen/YbNazJ",
-          })
-          .then(() => {
-            console.log("Thanks for sharing!");
-          })
-          .catch(console.error);
-      } else {
-        $menu.toggleClass("is-active");
-        $menuMask.toggleClass("is-active");
-        $body.css("overflow", "hidden");
-      }
-    });
-
-    // $menuToggle.on("click", function (e) {
-    //   e.preventDefault();
+    // shareButton.addEventListener("click", (event) => {
     //   if (navigator.share) {
     //     navigator
     //       .share({
@@ -43,6 +24,25 @@ $(document).ready(function () {
     //     $body.css("overflow", "hidden");
     //   }
     // });
+
+    $menuToggle.on("click", (event) => {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "WebShare API Demo",
+            url: "https://codepen.io/ayoisaiah/pen/YbNazJ",
+          })
+          .then(() => {
+            console.log("Thanks for sharing!");
+          })
+          .catch(console.error);
+      } else {
+        event.preventDefault();
+        $menu.toggleClass("is-active");
+        $menuMask.toggleClass("is-active");
+        $body.css("overflow", "hidden");
+      }
+    });
 
     $menuClose.on("click", function (e) {
       e.preventDefault();
