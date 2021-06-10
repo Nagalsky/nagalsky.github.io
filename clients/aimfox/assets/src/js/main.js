3b04eec2000db269bed8212fa943d4b4110573a8
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2021-06-10 17:56:09 +0300

    dwdw

---
+++ clients/aimfox/assets/src/js/main.js
@@ -0,0 +1,10 @@
+// Import jQuery
+//= ../../../node_modules/jquery/dist/jquery.min.js
+
+// Import Popper
+//= ../../../node_modules/@popperjs/core/dist/umd/popper.min.js
+
+// Import Bootstrap 5
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+// Import custom js-files
