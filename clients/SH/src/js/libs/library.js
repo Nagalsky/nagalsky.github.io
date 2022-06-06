299992d4938c0e2cd9853d07f7d6a5c122dad821
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-06-06 13:19:18 +0200

    Updates

--- clients/SH/src/js/libs/library.js
+++ clients/SH/src/js/libs/library.js
@@ -5,3 +5,4 @@
 //= ../../../node_modules/swiper/swiper-bundle.min.js
 //= ../../../node_modules/alpinejs/dist/cdn.min.js
 //= ../../../node_modules/aos/dist/aos.js
+//= ../../../node_modules/jquery-parallax.js/parallax.min.js
