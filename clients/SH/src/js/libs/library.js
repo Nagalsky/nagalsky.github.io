a43d34b7f2a737e76d9ded631f17aa6442dd3204
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-06-06 13:55:33 +0200

    remove parallax

--- clients/SH/src/js/libs/library.js
+++ clients/SH/src/js/libs/library.js
@@ -5,4 +5,3 @@
 //= ../../../node_modules/swiper/swiper-bundle.min.js
 //= ../../../node_modules/alpinejs/dist/cdn.min.js
 //= ../../../node_modules/aos/dist/aos.js
-//= ../../../node_modules/jquery-parallax.js/parallax.min.js
