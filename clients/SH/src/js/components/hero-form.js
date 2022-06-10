be9cf9806f46f1daf4d8d4ced3dd33a35977b15e
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-06-10 12:19:57 +0200

    added all the fixes

--- clients/SH/src/js/components/hero-form.js
+++ clients/SH/src/js/components/hero-form.js
@@ -1,54 +1,9 @@
 $(document).ready(function () {
   const el = $("#hero-form-select");
+  const hiddenIdsField = $("#array-ids");
   const apiURL = "https://www.stockhawk.io/api/instruments";
 
-  el.select2({
-    ajax: {
-      url: function (params) {
-        return `${apiURL}/${params.term}`;
-      },
-      dataType: "json",
-      delay: 250,
-      data: function () {
-        return null;
-      },
-      processResults: function (data) {
-        return {
-          results: $.map(data, function (e) {
-            return { id: e.instrumentId, name: e.name, symbol: e.symbol };
-          }),
-        };
-      },
-      cache: true,
-    },
-    theme: "hero-form-select",
-    width: $(this).data("width")
-      ? $(this).data("width")
-      : $(this).hasClass("w-full")
-      ? "100%"
-      : "style",
-    placeholder: "Enter tickers or companies to get started...",
-    closeOnSelect: false,
-    escapeMarkup: function (markup) {
-      return markup;
-    },
-    minimumInputLength: 1,
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection,
-    dropdownParent: $('.hero-form'),
-  });
-
-  el.on("change", function () {
-    var none = $(this).find("option:selected").length;
-
-    if (none === 0) {
-      $(".select2-search").removeAttr("style");
-    } else {
-      $(".select2-search").css('flex-grow', '0');
-    }
-  });
-
-  function formatRepo(repo) {
+  const formatRepo = (repo) => {
     if (repo.loading) {
       return repo.text;
     }
@@ -61,8 +16,42 @@ $(document).ready(function () {
 
     return markup;
   }
+  const formatRepoSelection = (repo) => repo.symbol || repo.text;
 
-  function formatRepoSelection(repo) {
-    return repo.name || repo.text;
-  }
+  el.select2({
+    ajax: {
+      url: (params) => `${apiURL}/${params.term}`,
+      dataType: "json",
+      delay: 250,
+      data: () => null,
+      processResults: (data) => {
+        return {
+          results: $.map(data, function (e) {
+            return { id: e.instrumentId, name: e.name, symbol: e.symbol };
+          }).sort((a, b) => a.symbol.localeCompare(b.symbol)),
+        };
+      },
+      cache: true,
+    },
+    escapeMarkup: (markup) => markup,
+    theme: "hero-form-select",
+    width: $(this).data("width")
+      ? $(this).data("width")
+      : $(this).hasClass("w-full")
+      ? "100%"
+      : "style",
+    placeholder: "Enter tickers or companies to get started...",
+    closeOnSelect: false,
+    minimumInputLength: 1,
+    templateResult: formatRepo,
+    templateSelection: formatRepoSelection,
+    dropdownParent: $('.hero-form'),
+  });
+
+  el.on("change", () => {
+    let arrIDs = [];
+    arrIDs = el.val();
+
+    hiddenIdsField.val(arrIDs.join(','));
+  });
 });
