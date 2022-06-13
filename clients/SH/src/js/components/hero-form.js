f086b73baa29bfb754def07de10012f7635eafa7
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-06-13 11:39:42 +0200

    added tom-select

--- clients/SH/src/js/components/hero-form.js
+++ clients/SH/src/js/components/hero-form.js
@@ -1,58 +1,47 @@
-$(document).ready(function () {
-  const el = $("#hero-form-select");
-  const hiddenIdsField = $("#array-ids");
-  const dropdownParent = $(".hero-form");
-  const searchField = $(".select2-search__field");
-  const apiURL = "https://www.stockhawk.io/api/instruments";
+const el = ("#hero-form-select");
+const heroForm = document.querySelector('.hero-form');
+const hiddenIdsField = document.querySelector("#array-ids");
+const apiURL = "https://www.stockhawk.io/api/instruments";
 
-  const formatRepo = (repo) => {
-    repo.loading ? dropdownParent.addClass('empty') : dropdownParent.removeClass('empty');
-    if (repo.loading) {
-      return repo.text;
-    }
+new TomSelect(el,{
+  valueField: 'instrumentId',
+  labelField: 'symbol',
+  searchField: ['symbol', 'name'],
+  sortField: [
+    { field: 'symbol', direction: 'asc'},
+    { field: 'name', direction: 'asc'}
+  ],
+  plugins: ['remove_button'],
+	persist: false,
+  onInitialize:() => {
+    heroForm.classList.remove('before-initialize');
+	},
+  onChange:(e) => {
+    hiddenIdsField.setAttribute('value', e.join(','));
+	},
+  load: function(query, callback) {
+    const url = `${apiURL}/${encodeURIComponent(query)}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        callback(json);
+      }).catch(()=>{
+        callback();
+      });
 
-    var markup = `
-      <div class='flex items-center'>
-        <div class='symbol' style='margin-right: 20px;'>${repo.symbol}</div>
-        <div class='name'>${repo.name}</div>
-      </div>`;
-
-    return markup;
-  }
-  const formatRepoSelection = (repo) => repo.symbol || repo.text;
-
-  el.select2({
-    ajax: {
-      url: (params) => `${apiURL}/${params.term}`,
-      dataType: "json",
-      delay: 250,
-      data: () => null,
-      processResults: (data) => {
-        return {
-          results: $.map(data, (e) => {
-            return { id: e.instrumentId, name: e.name, symbol: e.symbol };
-          }),
-        };
-      },
-      cache: true,
+  },
+  // custom rendering functions for options and items
+  render: {
+    option: function(item, escape) {
+      return `
+        <div class="flex items-center">
+          ${item.symbol && `<div style="margin-right: 20px;">${escape(item.symbol)}</div>`}
+          <div>${escape(item.name)}</div> 
+        </div>`;
     },
-    escapeMarkup: (markup) => markup,
-    width: '100%',
-    placeholder: "Enter tickers or companies to get started...",
-    closeOnSelect: false,
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection,
-    dropdownParent: dropdownParent,
-  }).on('select2:select', function (e) {
-    dropdownParent.addClass('hide-search');
-  }).on('select2:close', function (e) {
-    dropdownParent.removeClass('hide-search');
-  });
-
-  el.on("change", () => {
-    let arrIDs = [];
-    arrIDs = el.val();
-
-    hiddenIdsField.val(arrIDs.join(','));
-  });
+    item: function(item, escape) {
+      return `<div>${escape(item.symbol)}</div>`;
+    }
+  },
 });
+
--- clients/SH/src/js/libs/library.js
+++ clients/SH/src/js/libs/library.js
@@ -1,7 +1,6 @@
 //libraries like jquery etc
 
-//= ../../../node_modules/jquery/dist/jquery.min.js
-//= ../../../node_modules/select2/dist/js/select2.min.js
+//= ../../../node_modules/tom-select/dist/js/tom-select.complete.min.js
 //= ../../../node_modules/swiper/swiper-bundle.min.js
 //= ../../../node_modules/alpinejs/dist/cdn.min.js
 //= ../../../node_modules/aos/dist/aos.js
