41c86801444b5d01107532f699b5c999f5c11207
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-06-15 12:08:35 +0200

    added fixes

--- clients/SH/src/js/components/member-dashboard-datatable.js
+++ clients/SH/src/js/components/member-dashboard-datatable.js
@@ -2,14 +2,59 @@ $(document).ready(() => {
   const el = $("#member-dashboard-datatable");
   const tableParentBox = $(".datatable-holder");
   const tableSearchInput = $(".data-table-search-input");
+  const selectEl = "#member-dashboard-select";
+  const dashboardForm = document.querySelector(".member-dashboard-form");
+  const instrumentsApiURL = "https://www.stockhawk.io/api/instruments";
+  let selectedVal = null;
   const apiURL =
     "https://nagalsky.github.io/clients/ajax-data/member-dashboard.txt";
+  const bearerToken =
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkZzVnFVQUFPSTlhT1NkdFpLdFVzTCJ9.eyJodHRwczovL3d3dy5zdG9ja2hhd2suaW8vZW1haWwiOiJuYWdhbHNreS5uaWtpdGFAZ21haWwuY29tIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5zdG9ja2hhd2suaW8vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTIyNzI4NjE3MzExOTAzNzIzNzkiLCJhdWQiOlsiaHR0cHM6Ly93d3cuc3RvY2toYXdrLmlvIiwiaHR0cHM6Ly9kZXYtemgyaTBnZ2EudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY1NTI3OTM2NCwiZXhwIjoxNjU1MzY1NzY0LCJhenAiOiJkWnJ2NFpZTUJjMEJDRXdhOVB4UTFLZ0hab213a2tiQiIsInNjb3BlIjoib3BlbmlkIGVtYWlsIHN0b2NraGF3azp1c2Ugb2ZmbGluZV9hY2Nlc3MifQ.skCvo1Q88gpS67IEW3TQOqsFt1GrZSdVZvxo65RoyZZsaAcN0dxTqfr-qceht0K6-h7ulSZkV4na2pGp1bAHsmCuG0pruB5uZlVrJm-2mA-8eL3he2wznn8LqCApsLPCIbCbR_JdLNAMg3lnIrz57Bv4TLxh5ip8YdMRTz1jxiCKp7VQql2aP1vUvhC66sjq_1lnQw3EuEHnDBMt0hGFRIwsRFK1IJMj26b6I2pdZdVI1j_ozKTnmfd9rSTxiCHpUbhtMdYqnLx91zeJDVrMMInIHhqI9oX3cbuNYhHeCaRAkRgZSF4RSey0Bvb4Jl3cB9SFBFdApyHj1vTQ38q98w";
 
+  //Form for adding new tickers
+  if (typeof dashboardForm != "undefined" && dashboardForm != null) {
+    const select = new TomSelect(selectEl, {
+      valueField: "instrumentId",
+      labelField: "symbol",
+      searchField: ["symbol", "name"],
+      plugins: ["dropdown_input"],
+      onChange: (e) => {
+        selectedVal = e;
+        console.log("selectedVal+++ ", selectedVal);
+        select.clearOptions();
+      },
+      load: (query, callback) => {
+        const url = `${instrumentsApiURL}/${encodeURIComponent(query)}`;
+        fetch(url)
+          .then((response) => response.json())
+          .then((json) => {
+            callback(json);
+          })
+          .catch(() => {
+            callback();
+          });
+      },
+      render: {
+        option: (item, escape) => {
+          return `
+          <div class="flex items-center">
+            ${
+              item.symbol &&
+              `<div style="margin-right: 20px;">${escape(item.symbol)}</div>`
+            }
+            <div>${escape(item.name)}</div> 
+          </div>`;
+        },
+        item: (item, escape) => {
+          return `<div>${escape(item.symbol)}</div>`;
+        },
+      },
+    });
+  }
+
+  //Datatable
   const table = el.DataTable({
     ajax: apiURL,
-    initComplete: () => {
-      tableParentBox.removeClass("before-initialize");
-    },
     bSort: false,
     bInfo: false,
     dom: '<"table-responsive"t>',
@@ -18,39 +63,42 @@ $(document).ready(() => {
       { data: "companyName" },
       { data: "symbol" },
       {
-        mData: "amount",
-        mRender: (data) => {
+        data: null,
+        mRender: (row) => {
           return `
             <input
               type="text"
               placeholder="Amount"
-              value="${data}"
+              value="${row.amount}"
+              data-id="${row.id}"
               class="field-amount block w-full rounded border border-solid border-gray300 bg-white px-3 py-1.5 font-normal transition ease-in-out placeholder:text-gray400 focus:border-blue500 focus:outline-none"
             />
         `;
         },
       },
       {
-        mData: "openPrice",
-        mRender: (data) => {
+        data: null,
+        mRender: (row) => {
           return `
             <input
               type="text"
               placeholder="Open price"
-              value="${data}"
+              value="${row.openPrice}"
+              data-id="${row.id}"
               class="field-open-price block w-full rounded border border-solid border-gray300 bg-white px-3 py-1.5 font-normal transition ease-in-out placeholder:text-gray400 focus:border-blue500 focus:outline-none"
             />
         `;
         },
       },
       {
-        mData: "comment",
-        mRender: (data) => {
+        data: null,
+        mRender: (row) => {
           return `
             <textarea
               placeholder="Comment"
+              data-id="${row.id}"
               class="field-comment block w-full rounded border border-solid border-gray300 bg-white px-3 py-1.5 font-normal transition ease-in-out placeholder:text-gray400 focus:border-blue500 focus:outline-none resize-none"
-            >${data}</textarea>
+            >${row.comment}</textarea>
         `;
         },
       },
@@ -60,8 +108,8 @@ $(document).ready(() => {
         mRender: (row) => {
           return `
             <div class="text-center">
-              <button class="btn-delete p-3 text-red500" id="${row.id}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <button class="btn-delete p-3 text-red500" type="button" data-id="${row.id}">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                 </svg>
               </button>
@@ -71,28 +119,30 @@ $(document).ready(() => {
       },
     ],
     rowId: "id",
-    drawCallback: (e) => {
-      $(".field-amount").on("change", function () {
+    drawCallback: () => {
+      $(".field-amount").on("change", function (e) {
         console.log("field-amount value: ", $(this).val());
+        console.log("field-amount id: ", e.target.getAttribute("data-id"));
       });
-      $(".field-open-price").on("change", function () {
+      $(".field-open-price").on("change", function (e) {
         console.log("field-open-price value: ", $(this).val());
+        console.log("field-open-price id: ", e.target.getAttribute("data-id"));
       });
-      $(".field-comment").on("change", function () {
+      $(".field-comment").on("change", function (e) {
         console.log("field-comment value: ", $(this).val());
+        console.log("field-comment id: ", e.target.getAttribute("data-id"));
       });
-      $(".btn-delete").on("click", function () {
-        console.log("btn-delete value: ", e);
+      $(".btn-delete").on("click", function (e) {
+        tableDeleteRow(e.target.getAttribute("data-id"));
       });
     },
-  });
-
-  $("#member-dashboard-datatable tbody").on("click", "tr", function () {
-    var id = table.row(this).id();
-    alert("Clicked row id " + id);
   });
 
   tableSearchInput.on("keyup", function () {
     table.search(this.value).draw();
   });
+
+  function tableDeleteRow(id) {
+    console.log("tableDeleteRow id:", id);
+  }
 });
