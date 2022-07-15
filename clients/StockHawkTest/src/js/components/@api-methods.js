d8259ce6ebfa286194c63a59b45e156916c7cf13
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2022-07-15 16:42:27 +0200

    xw

--- clients/StockHawkTest/src/js/components/@api-methods.js
+++ clients/StockHawkTest/src/js/components/@api-methods.js
@@ -1,13 +1,6 @@
 const bearerToken =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkZzVnFVQUFPSTlhT1NkdFpLdFVzTCJ9.eyJodHRwczovL3d3dy5zdG9ja2hhd2suaW8vZW1haWwiOiJuYWdhbHNreS5uaWtpdGFAZ21haWwuY29tIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5zdG9ja2hhd2suaW8vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTIyNzI4NjE3MzExOTAzNzIzNzkiLCJhdWQiOlsiaHR0cHM6Ly93d3cuc3RvY2toYXdrLmlvIiwiaHR0cHM6Ly9kZXYtemgyaTBnZ2EudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY1NzgxNzMyNywiZXhwIjoxNjU3OTAzNzI3LCJhenAiOiJkWnJ2NFpZTUJjMEJDRXdhOVB4UTFLZ0hab213a2tiQiIsInNjb3BlIjoib3BlbmlkIGVtYWlsIHN0b2NraGF3azp1c2Ugb2ZmbGluZV9hY2Nlc3MifQ.H-_q3Z96oA-XoRIsfqQbJ-zWsg14IkaT6SHs9CduOGGCocyZfcliPQOT1x9apHEypsczHDQsZrOBC5jMwK5e3YW520gvGjzgkfI-62fBEtGBjt0AouchbcZ2105Dqjff46FfSFFhLUZsD8vx59Q9g9EDKi0MTokEWsxOmHW5JIQIS9SxrbwjnfLtrfjA7oPzcVBBH60aL3nHM4vt4u5wdtrAsIHag_h671QaUuFGvnOZUhcmk-n5J-h1Li1YmkX3hlFmWMDht8LelDO8Rf-EzyvY4PdpDSQLM6GbUP2Mqt290W4CpGbcoeHzscSHJA_hO-SmuwXabasftAI7Ky6Gig";
 
-const myHeaders = new Headers({
-  Authorization: `Bearer ${bearerToken}`,
-  "Content-Type": "application/json",
-  Cookie:
-    "ApplicationGatewayAffinity=7a04715ad5252cab2e2e787b5376885f; ApplicationGatewayAffinityCORS=7a04715ad5252cab2e2e787b5376885f",
-});
-
 const configHeaders = {
   headers: {
     Authorization: `Bearer ${bearerToken}`,
@@ -15,28 +8,16 @@ const configHeaders = {
   },
 };
 
-const requestGetOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow",
-};
-const requestDeleteOptions = {
-  method: "DELETE",
-  headers: myHeaders,
-  redirect: "follow",
-};
-const requestPatchOptions = {
-  method: "PATCH",
-  headers: myHeaders,
-  redirect: "follow",
-};
-const requestPutOptions = {
-  method: "PUT",
-  headers: myHeaders,
-  redirect: "follow",
-};
-const requestPostOptions = {
-  method: "POST",
-  headers: myHeaders,
-  redirect: "follow",
-};
+// axios.interceptors.response.use(
+//   function (response) {
+//     // Do something with response data
+//     return response;
+//   },
+//   function (error) {
+//     // Do something with response error
+//     if (error.response.status === 401) {
+//       console.log("Unauthorized Request");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
---
+++ clients/StockHawkTest/src/js/components/dashboard-build-watchlist-modal-add-watchlist.js
@@ -0,0 +1,184 @@
+function addWatchListModal() {
+  return {
+    watchlistName: "",
+    isWatchlistCreated: false,
+    search: "",
+    instruments: [],
+    selectedInstruments: [],
+    watchlistId: null,
+    noInstruments: false,
+    isLoading: false,
+    submitButtonText: "Save",
+
+    createWatchList() {
+      if (this.watchlistName === "") {
+        return;
+      }
+
+      const payload = {
+        Name: this.watchlistName.trim(),
+      };
+
+      NProgress.start();
+
+      this.isLoading = true;
+      this.submitButtonText = `<span class="flex items-center space-x-4"
+        ><svg
+          class="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path></svg
+        >Processing...</span>`;
+
+      axios
+        .post(`${watchListsApiUrl}`, payload, configHeaders)
+        .then((res) => {
+          this.isWatchlistCreated = true;
+          console.log("res", res);
+        })
+        .catch((err) => {
+          Toastify({
+            text: err?.response?.statusText,
+            ...toastDangerConfig,
+          }).showToast();
+        })
+        .finally(() => {
+          NProgress.done();
+          this.isLoading = false;
+          this.submitButtonText = "Save";
+        });
+    },
+
+    fetchInstruments() {
+      if (this.search === "") {
+        this.instruments = [];
+        this.noInstruments = false;
+        return;
+      }
+
+      NProgress.start();
+
+      axios
+        .get(`${instrumentsApiURL}/${this.search}`)
+        .then((res) => {
+          !res.data.length
+            ? (this.noInstruments = true)
+            : (this.noInstruments = false);
+
+          this.instruments = res.data;
+
+          this.instruments = this.instruments.filter(
+            (arr1) =>
+              !this.selectedInstruments.some(
+                (arr2) => arr1.instrumentId === arr2.instrumentId
+              )
+          );
+        })
+        .catch((err) => {
+          Toastify({
+            text: err?.response?.statusText,
+            ...toastDangerConfig,
+          }).showToast();
+        })
+        .finally(() => {
+          NProgress.done();
+        });
+    },
+
+    selectInstrument(obj) {
+      console.log("selectInstrument", obj);
+      const arr = [obj].flat() || [];
+
+      this.selectedInstruments = [...this.selectedInstruments, ...arr];
+    },
+
+    onDeleteSelectInstrument(id) {
+      this.selectedInstruments = this.selectedInstruments.filter(
+        (el) => el.instrumentId !== id
+      );
+      this.instruments = this.instruments.filter(
+        (arr1) =>
+          !this.selectedInstruments.some(
+            (arr2) => arr1.instrumentId === arr2.instrumentId
+          )
+      );
+    },
+
+    onSubmit() {
+      const payload = this.selectedInstruments.map((item) => ({
+        instrumentid: item.instrumentId,
+      }));
+
+      this.isLoading = true;
+      this.submitButtonText = `<span class="flex items-center space-x-4"
+        ><svg
+          class="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path></svg
+        >Processing...</span>`;
+
+      axios
+        .post(
+          `${watchListEntriesListApiUrl}/${this.watchlistId}`,
+          payload,
+          configHeaders
+        )
+        .then(() => {
+          Toastify({
+            text: "Watch list was created successfully!",
+            ...toastSuccessConfig,
+          }).showToast();
+          this.initialData();
+          this.showModalAddwatchlist = false;
+          this.search = "";
+          this.selectedInstruments = [];
+        })
+        .catch((err) => {
+          Toastify({
+            text: err?.response?.statusText,
+            ...toastDangerConfig,
+          }).showToast();
+        })
+        .finally(() => {
+          this.isLoading = false;
+          this.submitButtonText = "Save";
+        });
+    },
+
+    cancel() {
+      this.showModalAddwatchlist = false;
+      this.search = "";
+      this.selectedInstruments = [];
+      this.isWatchlistCreated = false;
+    },
+  };
+}
