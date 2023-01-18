a605b4c276094691f73450cb77408053409063ff
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2023-01-18 21:26:00 +0100

    Fix theme

--- clients/doctors-eyes/tailwind.config.js
+++ clients/doctors-eyes/tailwind.config.js
@@ -22,26 +22,11 @@ module.exports = {
         DEFAULT: "32px",
       },
     },
-    extend: {},
-  },
-  variants: {
     extend: {
-      // colors: {
-      //   "semi-white": "#eee8e1",
-      //   "dark-500": "#1b2022",
-      //   "purple-400": "#AD8AFF",
-      //   "purple-500": "#825AFF",
-      //   "purple-600": "#6540da",
-      //   "yellow-400": "#efd475",
-      //   "yellow-500": "#edc94a",
-      //   "yellow-600": "#d2b343",
-      //   "sky-500": "#75a8ff",
-      //   "sky-600": "#8eb8ff",
-      //   "rose-400": "#fa6898",
-      //   "rose-500": "#e35484",
-      //   "red-500": "#e8002d",
-      //   "green-500": "#249775",
-      // },
+      colors: {
+        "gray-500": "#f6f6f6",
+        "dark-500": "#232323",
+      },
       // backgroundImage: {
       //   "body-gradient":
       //     "linear-gradient(to top, #484b4b 0%, #252a2b 53%, #1b2022 100%)",
@@ -49,7 +34,7 @@ module.exports = {
       // },
       borderRadius: {
         DEFAULT: "16px",
-        "xl": "60px",
+        xl: "60px",
       },
     },
   },
