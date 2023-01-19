99eeed5ba43d5f9be7897746b306f17ab8ef9136
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2023-01-19 19:14:56 +0100

    added new comm

--- clients/doctors-eyes/src/js/libs/library.js
+++ clients/doctors-eyes/src/js/libs/library.js
@@ -1,5 +1,4 @@
 //libraries like jquery etc
 
 //= ../../../node_modules/alpinejs/dist/cdn.min.js
-//= ../../../node_modules/keen-slider/keen-slider.js
 //= ../../../node_modules/swiper/swiper-bundle.min.js
