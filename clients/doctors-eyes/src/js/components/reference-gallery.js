34943eb7976b2eb189b7d9cfb75533876da414a6
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2023-01-20 12:19:23 +0100

    dwdw

---
+++ clients/doctors-eyes/src/js/components/reference-gallery.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper("#reference-slider", {
+  slidesPerView: 1.4,
+  spaceBetween: 32,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2.2,
+      spaceBetween: 32,
+    },
+  },
+});
