d15146fb9692e97e8be6b9bd591e6377c0948c36
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2023-01-24 15:40:07 +0100

    dd

--- clients/write/src/js/components/gallery-testimonials.js
+++ clients/write/src/js/components/gallery-testimonials.js
@@ -1,6 +1,10 @@
 var swiper = new Swiper("#gallery-testimonials", {
   spaceBetween: 16,
   loop: true,
+  effect: "fade",
+  slidersPerView: 1,
+  fadeEffect: { crossFade: true },
+  virtualTranslate: true,
   autoplay: {
     delay: 3500,
     disableOnInteraction: false,
