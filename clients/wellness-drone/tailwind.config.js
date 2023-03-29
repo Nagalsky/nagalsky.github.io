d85e49afb480e5b4a83cc5015221d7b6f709a338
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2023-03-29 13:52:01 +0200

    added new project

---
+++ clients/wellness-drone/tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    screens: {
+      md: "768px",
+      lg: "991px",
+      xl: "1230px",
+      xxl: "1350px",
+    },
+    extend: {
+      fontFamily: {
+        default: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: {
+          200: "#f9f9f9",
+          500: "#3a3a3a",
+        },
+        green: {
+          500: "#17cea2",
+          600: "#16b992",
+        },
+      },
+    },
+  },
+};
