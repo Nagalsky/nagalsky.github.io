71a4569897149457037bab288c5b73ae6d799bd0
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2025-06-13 11:09:37 +0200

    removed old

--- clients/13-final-with-pages/src/app/(dashboard)/layout.tsx
+++
@@ -1,7 +0,0 @@
-interface AuthLayoutProps {
-  children: React.ReactNode
-}
-
-export default function DashboardLayout({ children }: AuthLayoutProps) {
-  return <div className="min-h-screen"><header>header only</header><p>Im a dashboard</p>{children}</div>
-}
--- clients/doctors-eyes/src/js/libs/library.js
+++
@@ -1,4 +0,0 @@
-//libraries like jquery etc
-
-//= ../../../node_modules/alpinejs/dist/cdn.min.js
-//= ../../../node_modules/swiper/swiper-bundle.min.js
