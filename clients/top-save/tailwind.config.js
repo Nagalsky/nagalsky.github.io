ba5f8cb4656dd1d54ad58972d10701231b8b1f9a
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2025-08-13 11:29:28 +0200

    added new Evelina's app

---
+++ clients/top-save/tailwind.config.js
@@ -0,0 +1,48 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "930px",
+      xl: "1218px",
+    },
+    extend: {
+      fontFamily: {
+        default: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: {
+          200: "#F7F2ED",
+          500: "#E55727",
+        },
+        yellow: {
+          500: "#eba605",
+        },
+        green: {
+          500: "#008100",
+        },
+        gray: {
+          100: "#f5f5f5",
+          200: "#f8f8f8",
+          300: "#efefef",
+          400: "#7f7f7f",
+          500: "#dedede",
+          600: "#7D7D7D",
+          700: "#ececec",
+          800: "#e9ecf2",
+          900: "#261b1b",
+        },
+      },
+    },
+  },
+};
